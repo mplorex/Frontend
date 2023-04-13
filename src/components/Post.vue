37a9b7efe0e6e06b739473e9da7d42c2b7d810cf
michael posselt <michaelposselt@outlook.com>
2023-04-13 13:45:13 -0500

    marked read functionality finishing touches

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,14 +3,26 @@
     <div class="form_tx1"><h3>{{ title }}</h3></div>
     <div class="form_tx2"><p>{{ description }}</p></div>   
     <img class="form_tx3" :src="imageUrl" />
-    <strong v-if="isRead">Post read</strong>
-    <button @click="markRead">Mark read</button>    
+    <div>
+        <button @click="read = !read">Mark read</button> 
+        <div>
+            <h1 v-if="read">Post has not been read</h1>   
+            <h1 v-else>Post has been read!</h1>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
 import {mapState} from 'pinia'
+import {useUserStore} from '../store/user'
+
 export default {
+    data() {
+        return {
+            read: true
+        }
+    },
     props: {
         title: String,
         description: String,
@@ -19,26 +31,13 @@ export default {
     computed: {
         ...mapState (useUserStore, ['user']), 
     },
-    data() {
-        return {
-            isRead: false,
-        }
-    },
     mounted () {
-        fetch('http://localhost:300/api/post' + this.user + '/read')
+        fetch('http://localhost:3000/api/posts/')
         .then(response => {
             if (response.ok) {
                 return response.json()
             }
         })
-        .then(isRead => {
-            this.isRead = isRead
-        })
-    },
-    methods: {
-        markRead() {
-            this.isRead = true
-        }
     }
 }
 </script>
@@ -63,7 +62,7 @@ export default {
     margin: auto;
     margin-top: 25px;
     width: 370px;
-    height: 300px;
+    height: 330px;
     border: bisque 1px transparent;
     box-shadow: 0 0px 0px 0 rgba(255, 112, 112, 0.884), 0 3px 12px 0 rgb(255, 99, 99);
     border-radius: 10px;
